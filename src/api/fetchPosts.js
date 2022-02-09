780e1aeddff4d486ca003a1d274035f77335d15e
Akshat Sethi <masterakshata@gmail.com>
2022-02-09 12:56:13 +0530

    Done

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,7 +1,5 @@
 const fetchPosts = async (page, limit) => {
-    const raw = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`)
-    const data = await raw.json()
-    return data
+    // optionally write fetching logic here or somewhere else
 }
 
 export { fetchPosts }
